13627261193e388222712e3108d0f4bd3ffc9e89
colbygllhr <cjg0033@mix.wvu.edu>
2026-05-28 11:45:57 -0400

    Added functionality for user input and the ability to have full conversations.

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
+import argparse
 import os
+
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-if api_key is None:
-    raise RuntimeError("API key not found.")
+def main() -> None:
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    args = parser.parse_args()
 
-client = genai.Client(api_key=api_key)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
-response = client.models.generate_content(
-    model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
-print(response.text)
+    client = genai.Client(api_key=api_key)
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+    generate_content(client, messages)
+
+
+def generate_content(client: genai.Client, messages: list[types.Content]) -> None:
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+    )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
